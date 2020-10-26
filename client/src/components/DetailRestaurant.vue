fe81543b60b011f34a8febcf50e42f1ae9198fb2
alphailosia <steph13.desire@gmail.com>
2020-10-26 15:16:37 +0100

    ajout de la map pour localiser le restaurant

--- client/src/components/DetailRestaurant.vue
+++ client/src/components/DetailRestaurant.vue
@@ -3,7 +3,18 @@
     <h1>Détail du restaurant : </h1>
     <p>Nom : {{ resto.name }}</p>
     <p>Cuisine : {{ resto.cuisine }}</p>
+    <p>Note : {{ resto.note }}</p>
     <p>Ville : {{ resto.borough }}</p>
+    <iframe
+      width="100%"
+      height="400px"
+      frameborder="0"
+      scrolling="yes"
+      marginheight="0"
+      marginwidth="0"
+      :src="url"
+      style="border-radius: 20px"
+    ></iframe>
   </v-container>
 </template>
 
@@ -12,6 +23,7 @@ export default {
   name: "restaurant",
   data: () => ({
     resto: {},
+    url: ""
   }),
   computed: {
     id() {
@@ -29,14 +41,28 @@ export default {
         .then((responseJSON) => {
           responseJSON.json().then((resJS) => {
             // Maintenant resJS est un vrai objet JavaScript
-            console.log(resJS);
+            //console.log(resJS);
             this.resto = resJS.restaurant;
+            console.log(this.resto);
+            this.geolocate();
           });
         })
         .catch((err) => {
           console.log(err);
         });
     },
+    geolocate: function () {
+      let size = 0.003;
+      let lat = this.resto.address.coord[1];
+      let lgn = this.resto.address.coord[0];
+
+      let lgnup = lgn + size;
+      let lgndown = lgn - size;
+      let latleft = lat - size;
+      let latright = lat + size;
+
+      this.url = `http://www.openstreetmap.org/export/embed.html?bbox=${lgndown}%2C${latleft}%2C${lgnup}%2C${latright}&layer=mapnik&marker=${lat}%2C${lgn}`;
+    }
   },
 };
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -18,6 +18,10 @@ const router = new VueRouter({
     {
       path: '/restaurant/:id',
       component: DetailRestaurant
+    },
+    {
+      path: '/map',
+      component: DetailRestaurant
     }
   ],
   mode: 'history'
